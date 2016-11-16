bfdf545696976fe3a631b0eae6cdedb2ef8c307a
roglos <roelmann@glos.ac.uk>
2016-11-16 07:49:31 +0000

    remove course header code from lib scss processing

--- lib.php
+++ lib.php
@@ -50,53 +50,7 @@ function theme_stagetwo_get_extra_scss($theme) {
 
     // Set the background image for the header.
     $headerbg = $theme->setting_file_url('courseimagedefaultheader', 'courseimagedefaultheader');
-
-global $CFG, $COURSE;
-
-if (empty($CFG->courseoverviewfileslimit)) {
-    return array();
-}
-require_once($CFG->libdir. '/filestorage/file_storage.php');
-require_once($CFG->dirroot. '/course/lib.php');
-$fs = get_file_storage();
-
-//--------------------------------------
-$context = context_course::instance($COURSE->id); // ERROR HERE: picking up $COURSE->ID = 1 on all courses.
-//--------------------------------------
-
-$files = $fs->get_area_files($context->id, 'course', 'overviewfiles', false, 'filename', false);
-if (count($files)) {
-    $overviewfilesoptions = course_overviewfiles_options($COURSE->id);
-    $acceptedtypes = $overviewfilesoptions['accepted_types'];
-    if ($acceptedtypes !== '*') {
-        // Filter only files with allowed extensions.
-        require_once($CFG->libdir. '/filelib.php');
-        foreach ($files as $key => $file) {
-            if (!file_extension_in_typegroup($file->get_filename(), $acceptedtypes)) {
-                unset($files[$key]);
-            }
-        }
-    }
-    if (count($files) > $CFG->courseoverviewfileslimit) {
-        // Return no more than $CFG->courseoverviewfileslimit files.
-        $files = array_slice($files, 0, $CFG->courseoverviewfileslimit, true);
-    }
-}
-
-// Display course overview files.
-$courseimage = '';
-foreach ($files as $file) {
-    $isimage = $file->is_valid_image();
-    if ($isimage) {
-        $courseimage = file_encode_url("$CFG->wwwroot/pluginfile.php",
-            '/'. $file->get_contextid(). '/'. $file->get_component(). '/'.
-            $file->get_filearea(). $file->get_filepath(). $file->get_filename(), !$isimage);
-    }
-}
-
     $extrascss .= 'header#page-header .card {background-image: url("'.$headerbg.'");}';
-    $extrascss .= 'header#page-header .card h1:after{content:": '.$COURSE->id.' : '.$context->id.' : '.count($files).$files.'"}';
-
 
     return $extrascss;
 }
